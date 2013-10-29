501c8844ddca9ec56ae35c73f684f37752c30c28
Griff <griff@thehamlins.org>
2013-10-29 17:24:16 -0500

    update README

--- rfm69rptr.ino
+++ rfm69rptr.ino
@@ -9,12 +9,27 @@
    sketch, data bytes received from the computer via the Serial connection are 
    transmitted using the moteino's wireless transmitter.  Data bytes received 
    from the moteino's wireless receiver is sent to the computer via the Serial 
-   connection.  You can put this sketch in two moteinos connected to two
-   computers each running a "terminal" program to create a "wireless" serial
-   connection between the computers. What you type on one computer
-   will be displayed on the other computer. I've tested it using
-   "terminal" programs minicom (on Linux) and PuttyTel (on Windows).  This 
-   sketch uses these libraries:
+   connection.  
+
+   You can put this sketch in two moteinos connected to two computers each 
+   running a "terminal" program to create a "wireless" serial connection 
+   between the computers. What you type on one computer will be displayed 
+   on the other computer. I've tested it using "terminal" programs minicom 
+   (on Linux) and HyperTerminal and PuttyTel (on Windows).  
+
+   Alternatively, you should be able attach a virtual network interface using 
+   the SLIP protocol to the serial port connected to the modeino on each 
+   computer.  Then your two computers are networked using IP, and you can use 
+   ALL the networking software you have (ping, ssh, telnet, ftp, email, 
+   a web browser, etc.) over the wireless link. Be aware the wireless 
+   radio can only handle 61 bytes of data per transmission (64 byte hardware
+   queue less 3 bytes overhead). You'll need to fix-up whatever SLIP driver
+   you're using to reduce the maximum packet length to 61. Also, the wireless 
+   connection is "simplex", using only one RF frequency, so there will be 
+   some IP packet loss due to RF "collisions" which will slow down the 
+   effective data rate. Also the IP packet overhead slows the effective rate.
+  
+   This sketch uses these libraries:
         RFM69 (https://github.com/LowPowerLab/RFM69) to control the 
                 moteino RF transceivers.
         SPIFlash (https://github.com/LowPowerLab/SPIFlash)
@@ -22,15 +37,14 @@
 
    My experiments show that the Serial.available() function on moteinos only 
    works if the "terminal" programs are set for software (Xon/Xoff) flow 
-   control. An Arduino Uno evidently doesn't have this requirement. This is 
-   unfortunate since this "repeater" sketch was written to provide  a
-   TCP/IP wireless link using SLIP. Evidently the SLIP software on my Linux 
-   computers uses hardware flow control. I tested the SLIP configuration by 
-   removing the two moteinos and wiring together the two FTDI boards connected 
-   to two computers. I crossed the TxD,RxD and the CTS/DTR connections. 
-   Without the CTS/DTR connections the SLIP configuration wouldn't work. With 
-   these connections, I was able to use all networking software (ssh, telnet, 
-   ping, etc..) between the two computers.
+   control. An Arduino Uno evidently doesn't have this requirement. 
+   SLIP software often uses hardware flow control over the serial device. 
+   So, after attaching a virtual SLIP interface to the serial device, you
+   need to enable software flow control on the device. In Linux, I  use
+   command "stty -F /dev/ttyUSB0 -crtscts ixon ixoff" to enable software
+   flow control, after I attach the virtual SLIP interface to the ttyUSB0
+   serial device.
+*/
 */
 #define NODEID      1
 #define NETWORKID   100
@@ -69,11 +83,12 @@ void loop() {
   if (n>0) {
     //get serial input
     nser=Serial.readBytes(inbuf,n);
-    // xmit these nser bytes and get an ACK. No retries.
-    ok=radio.sendWithRetry(theNodeID, (void *)inbuf, nser, 0); 
-    //if (!ok) {
-    //  Serial.print(nser); Serial.println(" bytes NOT sent ok");
-    //}
+    // xmit these nser bytes and get an ACK. 1 retries.
+    ok=radio.sendWithRetry(theNodeID, (void *)inbuf, nser, 1); 
+    /* if (!ok) {
+      Serial.print(nser); Serial.println(" bytes NOT sent ok");
+    } */
+    //radio.send(theNodeID, (void *)inbuf, nser, 0);  // send. no ACK
     Blink(LED,3);
   }  // end some serial data available
 
@@ -98,4 +113,5 @@ void Blink(byte PIN, int DELAY_MS)
   digitalWrite(PIN,HIGH);
   delay(DELAY_MS);
   digitalWrite(PIN,LOW);
+  delay(DELAY_MS); //GH
 }
